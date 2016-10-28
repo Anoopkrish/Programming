cb61f49ca962ae0a73ccbea478d9ac0d5ff67c6f
Anoopkrish <anoopkrish95@gmail.com>
2016-10-28 20:44:15 +0530

    Create #pattern.java

---
+++ #pattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,j;
+		for(i=1;i<=4;i++)
+		{
+			for(j=1;j<=4;j++)
+			{
+				if(i>=j)
+				System.out.print("#");
+			}System.out.println("");
+		}
+		// your code goes here
+	}
+}
